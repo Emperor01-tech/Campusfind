e93fdc8b051cd5c4b9b8eaddb28bc4907d9d0384
unknown <oluwasegundekoya@gmail.com>
2026-06-27 11:32:16 +0100

    Remove debug dbcheck endpoint

--- backend/routes.py
+++ backend/routes.py
@@ -136,10 +136,3 @@ def manual_seed():
 
 
 import os
-@routes_bp.route('/api/admin/dbcheck', methods=['GET'])
-def db_check():
-    from extensions import db
-    actual_engine_url = str(db.engine.url)
-    return jsonify({
-        'actual_engine_connected_to': actual_engine_url,
-    })
